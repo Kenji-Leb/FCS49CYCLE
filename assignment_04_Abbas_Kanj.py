5c2f502f4d0b5bd834028d23e928ece646b73142
Abbas Kanj <45877262+Kenji-Leb@users.noreply.github.com>
2023-11-22 13:07:49 +0200

    defining class

--- assignment_04_Abbas_Kanj.py
+++ assignment_04_Abbas_Kanj.py
@@ -3,3 +3,11 @@
 # Due Date: November 25, 10am
 # Name: Abbas Kanj
 #---------------------------------
+class taskManager:
+    
+    def __init__(self, task_id, description, priority, completed):
+        
+        self.taskId = task_id
+        self.description = description
+        self.prio = priority
+        self.completed = completed

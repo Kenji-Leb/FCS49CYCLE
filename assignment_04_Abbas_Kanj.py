ed1ed2628cb33bdc03b5bfc137f47bbde59eab0a
Abbas Kanj <45877262+Kenji-Leb@users.noreply.github.com>
2023-11-22 13:36:20 +0200

    Initializing menu and main

--- assignment_04_Abbas_Kanj.py
+++ assignment_04_Abbas_Kanj.py
@@ -3,7 +3,7 @@
 # Due Date: November 25, 10am
 # Name: Abbas Kanj
 #---------------------------------
-class taskManager:
+class task:
     
     def __init__(self, task_id, description, priority, completed):
         
@@ -11,6 +11,7 @@ class taskManager:
         self.description = description
         self.prio = priority
         self.comp = completed
+        
     # Initializing Getters
     def getTask(self):
         return self.taskId
@@ -23,6 +24,7 @@ class taskManager:
     
     def getComp(self):
         return self.comp
+    
     # Initializing Setters
     def setTask(self, new_task):
         self.taskId = new_task
@@ -35,4 +37,83 @@ class taskManager:
         
     def setComp(self, new_comp):
         self.comp = new_comp
+#-----------------------------------------------------
+def main():
+
+    while(True):
+
+        try:
+            displaymenu()
+            choice = int(input("Please select one of the options: "))
+        except:
+            print("Invalid Input....")
+            continue
+
+        if choice == 1:
+            
+            addNewTask
+
+        elif choice == 2:
+            
+            getById
+
+        elif choice == 3:
+            
+            markPrioComplete
+
+        elif choice == 4:
+            
+            displayTasks
+
+        elif choice == 5:
+           
+           displayNotCompTasks
+
+        elif choice == 6:
+            
+            displayLastTask
+
+        elif choice == 7:
+            exit
+#-----------------------------------------------------
+def displaymenu():
+    print("----------------------------")
+    print("1- Adding a new task to the task manager\n" + 
+          "2- Getting a task from the queue given a task id\n" + 
+          "3- Marking the highest priority task as completed and putting it in the task history\n" + 
+          "4- Displaying all tasks in order of priority\n" + 
+          "5- Displaying only the tasks that are not completed\n" + 
+          "6- Displaying the last completed task\n" + 
+          "7- Exit\n")
+    print("----------------------------")
+#-----------------------------------------------------
+def addNewTask():
     
+    print()
+
+#-----------------------------------------------------
+def getById():
+    
+    print()
+
+#-----------------------------------------------------
+def markPrioComplete():
+    
+    print()
+
+#-----------------------------------------------------
+def displayTasks():
+    
+    print()
+
+#-----------------------------------------------------
+def displayNotCompTasks():
+    
+    print()
+
+#-----------------------------------------------------
+def displayLastTask():
+    
+    print()
+
+#-----------------------------------------------------
